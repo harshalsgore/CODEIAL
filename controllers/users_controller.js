3081db5b8d5d3b9074e58820f03dd7903ee25ab3
harshal <harshalsgore@gmail.com>
2023-06-07 17:04:09 +0530

    Sign Up created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,53 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile',{
         title: "User Profile"
     })
+}
+
+//Render SignUp page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "User SignUp"
+    })
+}
+
+//Render SignIn page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "User SignIn"
+    })
+}
+
+//Get the SignUp data
+module.exports.create = function(req, res){
+    if(req.body.password!=req.body.Confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email})
+    .then((result) => {
+        if(!result){
+            User.create(req.body)
+            .then((result) => {
+                return res.redirect('/users/sign-in');
+            })
+            .catch((err) => {
+                console.log("Error in creating user", err);
+                return;
+            })
+        }else{
+            return res.redirect('back');
+        }
+     })
+     .catch((err) => {
+       console.log('Error in finding user',err);
+       return;
+     })
+
+}
+
+//Get the SignIn data
+module.exports.createSession = function(req, res){
+    
 }
