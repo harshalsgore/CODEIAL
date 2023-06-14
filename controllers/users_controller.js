0313c4dd50e442d20f6b7f2a8bc30e95e79d5c09
harshal <harshalsgore@gmail.com>
2023-06-14 10:28:24 +0530

    Used SASS

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const passport = require('passport');
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
@@ -8,6 +9,9 @@ module.exports.profile = function(req, res){
 
 //Render SignUp page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile')
+    }
     return res.render('user_sign_up',{
         title: "User SignUp"
     })
@@ -15,6 +19,9 @@ module.exports.signUp = function(req, res){
 
 //Render SignIn page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile')
+    }
     return res.render('user_sign_in',{
         title: "User SignIn"
     })
@@ -47,7 +54,14 @@ module.exports.create = function(req, res){
 
 }
 
-//Get the SignIn data
+//SignIn and create session
 module.exports.createSession = function(req, res){
-    
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        return res.redirect('/');
+      });
 }
